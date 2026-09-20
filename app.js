8fd6740e7b45db8ccf7b99e717f35cb9594ceb13
Akash gautam <cfact6507@gmail.com>
2026-09-20 17:23:16 +0530

    update  app.js file add keydown Event

--- app.js
+++ app.js
@@ -31,3 +31,40 @@ buttonsArray.forEach((btn) => {
 
     })
 })
+
+document.addEventListener("keydown",(e)=>{
+    if (e.key >= "0" && e.key <= "9"){
+        string += e.key;
+        updateDisplay()
+    }
+    
+    else if(["+","-","*","/","%"].includes(e.key)){
+        string += e.key;
+        updateDisplay();
+    }
+
+    else if(e.key === "."){
+        string  += e.key;
+        updateDisplay();
+    }
+
+     else if (e.key === "Enter" || e.key === "=") {
+        try {
+            string = eval(string);
+            updateDisplay();
+        } catch {
+            string = "";
+            Display.value = "Error";
+        }
+    }
+
+     else if (e.key === "Backspace") {
+        string = string.substring(0, string.length - 1);
+        updateDisplay();
+    }
+
+    else if (e.key === "Escape" || e.key === "Delete") {
+        string = "";
+        updateDisplay();
+    }
+})
